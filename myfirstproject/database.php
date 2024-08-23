9e6729fcb06ea69efd8cfb6bfcdad1413e9ca221
Gotofud <pajlisukabuah@gmail.com>
2024-08-23 21:12:38 +0700

    Tugas 4

---
+++ myfirstproject/database.php
@@ -0,0 +1,18 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "mydatabase";
+
+// Membuat koneksi
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Periksa koneksi
+if ($conn->connect_error) {
+    die("Koneksi gagal: " . $conn->connect_error);
+}
+// echo "Koneksi berhasil";
+
+// // Menutup koneksi
+// $conn->close();
+
---
+++ myfirstproject/index.php
@@ -0,0 +1,17 @@
+<?php
+include "database.php";
+
+echo "Hello Laragon";
+echo "<br>";
+
+$sql = "SELECT id, name, email FROM users";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    while($row = $result->fetch_assoc()) {
+        
+        echo "id: " . $row["id"]. " - Name: " . $row["name"]. " - Email: " . $row["email"]. "<br>";
+    }
+} else {
+    echo "0 results";
+}
